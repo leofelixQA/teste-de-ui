6f4e30ff333da20504358ade148664f3ea2c9f39
Leonardo <leofelix1983@gmail.com>
2026-07-30 09:05:03 -0300

    automação do contato

--- automação-cypress/ins-cypress/cypress/e2e/contato.cy.js
+++ automação-cypress/ins-cypress/cypress/e2e/contato.cy.js
@@ -1,5 +1,22 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
-  })
-})
+import { faker } from '@faker-js/faker';
+
+describe('funcionalidade do contato', () => {
+beforeEach(() => {
+  cy.visit("index.html")
+});
+it.only('deve preencher o formulario de contato com sucesso', () => {
+  let nome = faker.person.firstName()
+  let email = faker.internet.email()
+   cy.get('[name="name"]').type(nome) 
+   cy.get('[name="email"]').type(email)
+   cy.get('[name="subject"]').select("Parcerias")
+   cy.get('[name="message"]').type("mensagem")
+   cy.get('#btn-submit').click()
+   cy.contains("Contato enviado com sucesso!").should("exist")
+});
+  
+
+
+
+});
+  
