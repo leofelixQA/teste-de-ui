09f68fda02ffcbe553ce56a303babbed0dd0cb18
Leonardo <leofelix1983@gmail.com>
2026-08-07 16:25:31 -0300

    catalogo-busca

--- automação-cypress/ins-cypress/cypress/e2e/catalogo-busca.cy.js
+++ automação-cypress/ins-cypress/cypress/e2e/catalogo-busca.cy.js
@@ -10,8 +10,25 @@ describe('funcionalidade: catalogo de busca', () => {
         cy.get('.card-title').should("contain","1984")
     });
 
-    it.only('deve fazer a busca de um livro do catalogo', () => {
+    it('deve fazer a busca de um livro do catalogo', () => {
         cy.get('#search-input').type(catalogo[2].livro)
         cy.get('.card-title').should("contain",catalogo[2].livro)
+    });
+
+    it('deve fazer a busca de um livro usando fixture', () => {
+        cy.fixture('livros').then((cat)=>{
+         cy.get('#search-input').type(cat[2].livro)
+         cy.get('.card-title').should("contain",cat[2].livro)
+
+    })
+    });
+
+    it.only('deve fazer a busca de todos o livros do catalogo', () => {
+        cy.fixture("livros").then((cat)=>{
+            cat.forEach(item=>{
+              cy.get('#search-input').clear().type(item.livro)
+              cy.get('.card-title').should("contain",item.livro)  
+            })
+        })
     });
 });
